00815fae3e47721cdf1c7e2a302070a2c0f21b16
tam.nguyen <tam.nguyen@zodinet.com>
2025-10-11 23:33:16 +0700

    update: Change greeting message in app.js and switch to cloudflared GitHub Action in deploy.yaml

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const apiKey = process.env.API_KEY || 'No API Key set';
 
 app.get('/', (req, res) => {
   res.json({
-    message: "Hello from my CI/CD pipeline!",
+    message: "Hello from my CI/CD pipeline V2!",
     hostname: req.hostname,
     podName: process.env.HOSTNAME,
     // Trả về cấu hình đã đọc được
