1ae2725d7cf92d23ce0470f5328d9da56692b4b4
tam.nguyen <tam.nguyen@zodinet.com>
2025-10-12 21:01:41 +0700

    feat: Integrate PostgreSQL database support in app.js with CRUD operations for messages, update Docker image tag in hello-app.yaml, and add pg dependency in package.json

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const { Pool } = require('pg'); // Import thư viện pg
+
 const app = express();
 const PORT = 3000;
 
 // Đọc cấu hình từ biến môi trường
 const apiUrl = process.env.API_URL || 'No API URL set';
 const apiKey = process.env.API_KEY || 'No API Key set';
+
+// --- CẤU HÌNH KẾT NỐI DATABASE ---
+// Đọc thông tin kết nối từ các biến môi trường do Kubernetes cung cấp
+const dbPool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+});
+
 
 app.get('/', (req, res) => {
   res.json({
@@ -18,6 +31,57 @@ app.get('/', (req, res) => {
     }
   });
 });
+
+// INSEERT NEW MESSAGE TO DATABASE
+app.get('/messages', async (req, res) => {
+  try {
+    const client = await dbPool.connect();
+    const result = await client.query('SELECT * FROM messages');
+    client.release(); // Trả kết nối về pool
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error executing query', err.stack);
+    res.status(500).json({ error: 'Failed to fetch messages from database' });
+  }
+});
+
+// POST MESSAGE
+app.post('/messages', async (req, res) => {
+  const { message } = req.body;
+  const client = await dbPool.connect();
+  const result = await client.query('INSERT INTO messages (message) VALUES ($1)', [message]);
+  client.release();
+  res.json(result.rows);
+});
+
+// UPDATE MESSAGE
+app.put('/messages/:id', async (req, res) => {
+  const { id } = req.params;
+  const { message } = req.body;
+  const client = await dbPool.connect();
+  const result = await client.query('UPDATE messages SET message = $1 WHERE id = $2', [message, id]);
+  client.release();
+  res.json(result.rows);
+});
+
+// GET MESSAGE BY ID
+app.get('/messages/:id', async (req, res) => {
+  const { id } = req.params;
+  const client = await dbPool.connect();
+  const result = await client.query('SELECT * FROM messages WHERE id = $1', [id]);
+  client.release();
+  res.json(result.rows);
+});
+
+// DELETE MESSAGE
+app.delete('/messages/:id', async (req, res) => {
+  const { id } = req.params;
+  const client = await dbPool.connect();
+  const result = await client.query('DELETE FROM messages WHERE id = $1', [id]);
+  client.release();
+  res.json(result.rows);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
